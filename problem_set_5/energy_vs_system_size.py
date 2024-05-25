894c90e1fb68e43600241fe4d8a11f72d95c2982
Leonard <L.Pingen@physik.uni-muenchen.de>
2024-05-25 16:11:22 +0200

    large system size

--- problem_set_5/energy_vs_system_size.py
+++ problem_set_5/energy_vs_system_size.py
@@ -3,14 +3,14 @@ import numpy as np
 import math
 from xy_model import XYmodel
 
-system_sizes = np.arange(2, 200, 2)
+system_sizes = np.logspace(1, 14, 30, base=2, dtype=int)
 energies = []
 
 for run, n in enumerate(system_sizes):
     model = XYmodel(n)
     model.place_vortex(math.floor(n/2) + .5, math.floor(n/2) + .5, 1)
     energies.append(model.compute_energy() - model.energy0)
-    if run % 50 == 0:
+    if run % 5 == 0:
         print(f"{run} / {len(system_sizes)}")
 
 fig, ax = plt.subplots(dpi=500)
